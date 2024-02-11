34b1c376222a3446fae36115ccfa11818bf1f15c
dean-ferreira <deanferreira20@gmail.com>
2024-02-11 15:00:55 -0600

    Implement API functions

---
+++ src/script.js
@@ -0,0 +1,39 @@
+import { getPosts } from './api.js';
+
+const contentTitle = document.querySelector('#content-title');
+const postsContainer = document.querySelector('.posts-container');
+const postTileTemplate = document.querySelector('#post-tile-template');
+
+const homeLink = document.querySelector('#home-link');
+const allPostsLink = document.querySelector('#all-posts');
+
+function createPostTile(post) {
+    const postTile = postTileTemplate.content.cloneNode(true);
+    postTile.querySelector('.post-title').innerText = post.title;
+    postTile.querySelector('.post-content').innerText = post.body;
+    postsContainer.appendChild(postTile);
+}
+
+async function displayAllPosts() {
+    let allPosts = await getPosts();
+    contentTitle.innerText = 'All Posts';
+    postsContainer.innerHTML = '';
+    for (let post of allPosts) {
+        createPostTile(post);
+    }
+}
+
+async function displayLatestPosts() {
+    let allPosts = await getPosts();
+    contentTitle.innerText = 'Latest Posts';
+    postsContainer.innerHTML = '';
+    for (let i = allPosts.length - 1; i > allPosts.length - 6; i--) {
+        createPostTile(allPosts[i]);
+    }
+}
+
+async function init() {
+    displayLatestPosts();
+}
+
+init();
